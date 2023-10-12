088beb3755ee5e07607457cd67930bae72d49a73
Vishwanath Kurella <vishwa.kurella@icloud.com>
2023-10-13 03:04:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputText: "",
+    };
+  }
+
+  handleInputChange = (e) => {
+    const text = e.target.value;
+    this.setState({ inputText: text });
+  }
+
+  countWords = () => {
+    const words = this.state.inputText.split(/\s+/);
+    return words.length;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Responsive Word Counter</h1>
+          <textarea
+            placeholder="Type your text here..."
+            onChange={this.handleInputChange}
+          ></textarea>
+          <div className="word-count">
+            Word Count: {this.countWords()}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
